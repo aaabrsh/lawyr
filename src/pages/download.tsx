e4caa26e72c4b7aa7ab1449363de097198dc9eec
Abrham Alemayehu <aaabrham421@gmail.com>
2023-01-31 16:48:48 +0300

    added fieldset, made form responsive

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -164,7 +164,14 @@ export default function Welcome() {
         </Dialog>
       </Transition>
 
-      <body>
+      <body className="mx-10 my-4">
+        <h1 className="text-3xl font-extrabold text-gray-800">
+          Official Demand Letter
+        </h1>
+        <p className="mb-4 text-gray-500">
+          please answer the following questions to create an official demand
+          letter.
+        </p>
         <form className={formStyles.form} onSubmit={hanldeFormSubmit}>
           <div className={formStyles.inputs_container}>
             <div className="w-full">
@@ -192,6 +199,7 @@ export default function Welcome() {
               />
             </div>
           </div>
+
           <div className={formStyles.inputs_container}>
             <div className="w-full">
               <label htmlFor="date" className={formStyles.label}>
@@ -199,7 +207,7 @@ export default function Welcome() {
               </label>
               <div className="flex">
                 <input
-                  className={`${formStyles.input} ${formStyles.inline_input}`}
+                  className={formStyles.input}
                   type="text"
                   id="date"
                   name="Date"
@@ -228,7 +236,7 @@ export default function Welcome() {
               </label>
               <div className="flex">
                 <input
-                  className={`${formStyles.input} ${formStyles.inline_input}`}
+                  className={formStyles.input}
                   type="text"
                   id="last_date"
                   name="If there is no response to this demand letter by"
@@ -255,70 +263,78 @@ export default function Welcome() {
               </div>
             </div>
           </div>
-          <div className="mb-2">
+
+          <div className="mb-6">
             <label className={formStyles.label} htmlFor="reason">
               Reason for Letter
             </label>
-            <textarea
+            <input
+              type="text"
               className={formStyles.input}
               id="reason"
               name="undefined"
-              rows={2}
               required
             />
           </div>
-          <div className={formStyles.inputs_container}>
-            <div className="w-full">
-              <label className={formStyles.label} htmlFor="address1">
-                Street Address
-              </label>
-              <input
-                className={formStyles.input}
-                type="text"
-                id="address1"
-                name="From 1"
-              />
+
+          <fieldset className="mb-6 rounded border py-3 px-5">
+            <legend>&nbsp; Address Information &nbsp;</legend>
+            <div className={formStyles.inputs_container}>
+              <div className="w-full">
+                <label className={formStyles.label} htmlFor="address1">
+                  Street Address
+                </label>
+                <input
+                  className={formStyles.input}
+                  type="text"
+                  id="address1"
+                  name="From 1"
+                />
+              </div>
+              <div className="w-full">
+                <label className={formStyles.label} htmlFor="address2">
+                  Street Address (line 2)
+                </label>
+                <input
+                  className={formStyles.input}
+                  type="text"
+                  id="address2"
+                  name="From 2"
+                />
+              </div>
             </div>
-            <div className="w-full">
-              <label className={formStyles.label} htmlFor="address2">
-                Street Address (line 2)
-              </label>
-              <input
-                className={formStyles.input}
-                type="text"
-                id="address2"
-                name="From 2"
-              />
+            <div className={formStyles.inputs_container}>
+              <div className="w-full">
+                <label className={formStyles.label} htmlFor="city">
+                  City, State
+                </label>
+                <input
+                  className={formStyles.input}
+                  type="text"
+                  id="city"
+                  name="From 3"
+                />
+              </div>
+              <div className="w-full">
+                <label className={formStyles.label} htmlFor="zip_code">
+                  Zip Code
+                </label>
+                <input
+                  className={formStyles.input}
+                  type="text"
+                  id="zip_code"
+                  name="From 4"
+                />
+              </div>
             </div>
+          </fieldset>
+          <div className="flex justify-end">
+            <button className={formStyles.button} type="submit">
+              Download PDF
+            </button>
           </div>
-          <div className={formStyles.inputs_container}>
-            <div className="w-full">
-              <label className={formStyles.label} htmlFor="city">
-                City, State
-              </label>
-              <input
-                className={formStyles.input}
-                type="text"
-                id="city"
-                name="From 3"
-              />
-            </div>
-            <div className="w-full">
-              <label className={formStyles.label} htmlFor="zip_code">
-                Zip Code
-              </label>
-              <input
-                className={formStyles.input}
-                type="text"
-                id="zip_code"
-                name="From 4"
-              />
-            </div>
-          </div>
-          <button className={formStyles.button} type="submit">
-            Download PDF
-          </button>
         </form>
+
         <p>
           Click the button to modify an existing PDF document with{" "}
           <code>pdf-lib</code>
