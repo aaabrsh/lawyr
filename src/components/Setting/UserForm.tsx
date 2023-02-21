afdfc3bbe31ca331e3ca0e52a569de9dda3aec15
Abraham <aaabrham421@gmail.com>
2023-02-21 19:03:32 +0300

    added user setting form

---
+++ src/components/Setting/UserForm.tsx
@@ -0,0 +1,179 @@
+import { FormEvent, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function UserForm({ user, id }) {
+  const [userData, setUserData] = useState(user);
+  let router = useRouter();
+
+  const handleChange = (event: any) => {
+    setUserData((data: any) => ({
+      ...data,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await axios.put(`/api/users/${id}`, userData);
+    router.reload();
+  };
+
+  return (
+    <>
+      <div className="m-auto mt-5 max-w-lg md:col-span-2 md:mt-0">
+        <h1 className="mt-2 text-3xl font-extrabold text-zinc-800 sm:text-center">
+          Personal Information
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className="overflow-hidden shadow sm:rounded-md">
+            <div className="bg-white px-4 py-5 sm:p-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Full name
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 px-3 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                    value={userData.name}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="email-address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email-address"
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 px-3 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                    value={userData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="dob"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Date of Birth
+                  </label>
+                  <input
+                    type="date"
+                    name="dob"
+                    id="dob"
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 px-3 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                    value={userData.dob}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="country"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Country
+                  </label>
+                  <input
+                    type="text"
+                    name="country"
+                    id="country"
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 px-3 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                    value={userData.country}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label
+                    htmlFor="street-address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Street address
+                  </label>
+                  <input
+                    type="text"
+                    name="street_address"
+                    id="street-address"
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 px-3 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                    value={userData.street_address}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3 lg:col-span-3">
+                  <label
+                    htmlFor="city"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 px-3 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                    value={userData.city}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label
+                    htmlFor="region"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    State / Province
+                  </label>
+                  <input
+                    type="text"
+                    name="state"
+                    id="region"
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 px-3 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                    value={userData.state}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label
+                    htmlFor="zip-code"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    ZIP / Postal code
+                  </label>
+                  <input
+                    type="text"
+                    name="zip_code"
+                    id="zip-code"
+                    className="mt-1 block w-full rounded-md border border-gray-300 p-2 px-3 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                    value={userData.zip_code}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+              <button
+                type="submit"
+                className="inline-flex justify-center rounded-md border border-transparent bg-pink-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../server/db";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PUT") {
+    let { id } = req.query;
+    let userData = req.body;
+    userData.dob = new Date(userData.dob);
+    try {
+      const user = await prisma.user.update({
+        where: {
+          id: id,
+        },
+        data: {
+          ...userData,
+        },
+      });
+      res.send("user updated");
+    } catch (err) {
+      console.error(err);
+      res.send("couldn't update user");
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
