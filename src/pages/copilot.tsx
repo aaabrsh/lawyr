c218b8bc85bfe3090986cec3b190b2deb7d47b34
Natnael <natnaelgetenew@gmail.com>
2023-02-23 10:06:45 +0300

    ui fixes

--- src/pages/copilot.tsx
+++ src/pages/copilot.tsx
@@ -237,12 +237,57 @@ export default function Copilot({ plans }) {
                       <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" />
                     </div>
                   </div>
-                  <div className="bg-gray-300 p-4">
-                    <input
+                  <div className="bg-gray-100 p-4">
+                    {/* <input
                       className="flex h-10 w-full items-center rounded px-3 text-sm"
                       type="text"
                       placeholder="Type your message…"
-                    />
+                    /> */}
+                    <form>
+                      <label htmlFor="chat" className="sr-only">
+                        Your message
+                      </label>
+                      <div className="flex items-center rounded-lg bg-gray-50 py-2 px-3 dark:bg-gray-700">
+                        <button
+                          type="button"
+                          disabled="disabled"
+                          className="inline-flex cursor-pointer justify-center rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          <svg
+                            className="h-6 w-6"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </button>
+                        <textarea
+                          id="chat"
+                          rows={1}
+                          className="mx-4 block w-full rounded-lg border border-gray-300 bg-white p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                          placeholder="Your message..."
+                          defaultValue={""}
+                        />
+                        <button
+                          type="submit"
+                          className="inline-flex cursor-pointer justify-center rounded-full p-2 text-blue-600 hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
+                        >
+                          <svg
+                            className="h-6 w-6 rotate-90"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                          </svg>
+                        </button>
+                      </div>
+                    </form>
                   </div>
                 </div>
                 {/* </div> */}
