006422353be2a26d1cc0869f86f5d5832ff166a4
Abraham <aaabrham421@gmail.com>
2023-03-18 21:41:27 +0300

    added chat saving

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -20,7 +20,7 @@ import { toast } from "react-hot-toast";
 pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 
 // import axios from "axios";
-export default function Chat({ pdf_files }: any) {
+export default function Chat({ pdf_files, chat }: any) {
   const [isOpen, setIsOpen] = useState(false);
   const [files, setFiles] = useState([]);
   const [active, setActive] = useState(false);
@@ -39,6 +39,7 @@ export default function Chat({ pdf_files }: any) {
 
   useEffect(() => {
     getPdfFiles(pdf_files);
+    setChatHistory(chat);
   }, [pdf_files]);
 
   async function getPdfFiles(files: any[]) {
@@ -95,13 +96,12 @@ export default function Chat({ pdf_files }: any) {
 
   function handleChatInput() {
     let input = inputText;
+    let data = { sender: "user", text: input, userId: session?.user?.id };
     if (input !== "") {
-      setChatHistory((history) => [
-        ...history,
-        { sender: "user", text: input },
-      ]);
+      setChatHistory((history) => [...history, data]);
       chatWithOpenai(input);
       setInputText("");
+      saveChat(data);
     }
   }
 
@@ -117,14 +117,28 @@ export default function Chat({ pdf_files }: any) {
       }),
     };
 
-    let response = await fetch("http://localhost:8000/api/chat", requestOptions)
+    const domain = window.location.hostname;
+    const protocol = window.location.protocol;
+    const apiUrl = `${protocol}//${domain}:8000/api/chat`;
+
+    let response = await fetch(apiUrl, requestOptions)
       .then((res) => res.json())
       .catch((err) => console.log(err));
 
-    setChatHistory((history) => [
-      ...history,
-      { sender: "bot", text: response },
-    ]);
+    let data = { sender: "bot", text: response, userId: session?.user?.id };
+    setChatHistory((history) => [...history, data]);
+    saveChat(data);
+  }
+
+  function saveChat(data: any) {
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ data }),
+    };
+    fetch("/api/chat", requestOptions);
   }
 
   return (
@@ -248,200 +262,6 @@ export default function Chat({ pdf_files }: any) {
                   id="messages"
                   className="scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch flex flex-col space-y-4 overflow-y-auto p-3"
                 >
-                  {/* <div className="chat-message">
-                    <div className="flex items-end">
-                      <div className="order-2 mx-2 flex max-w-xs flex-col items-start space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg rounded-bl-none bg-gray-300 px-4 py-2 text-gray-600">
-                            Can be verified on any platform using docker
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-1 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="chat-message">
-                    <div className="flex items-end justify-end">
-                      <div className="order-1 mx-2 flex max-w-xs flex-col items-end space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg rounded-br-none bg-blue-600 px-4 py-2 text-white ">
-                            Your error message says permission denied, npm
-                            global installs must be given root privileges.
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-2 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="chat-message">
-                    <div className="flex items-end">
-                      <div className="order-2 mx-2 flex max-w-xs flex-col items-start space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg bg-gray-300 px-4 py-2 text-gray-600">
-                            Command was run with root privileges. Im sure about
-                            that.
-                          </span>
-                        </div>
-                        <div>
-                          <span className="inline-block rounded-lg bg-gray-300 px-4 py-2 text-gray-600">
-                            Ive update the description so its more obviously now
-                          </span>
-                        </div>
-                        <div>
-                          <span className="inline-block rounded-lg bg-gray-300 px-4 py-2 text-gray-600">
-                            FYI https://askubuntu.com/a/700266/510172
-                          </span>
-                        </div>
-                        <div>
-                          <span className="inline-block rounded-lg rounded-bl-none bg-gray-300 px-4 py-2 text-gray-600">
-                            Check the line above (it ends with a # so, Im
-                            running it as root )
-                            <pre># npm install -g @vue/devtools</pre>
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-1 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="chat-message">
-                    <div className="flex items-end justify-end">
-                      <div className="order-1 mx-2 flex max-w-xs flex-col items-end space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg rounded-br-none bg-blue-600 px-4 py-2 text-white ">
-                            Any updates on this issue? Im getting the same error
-                            when trying to install devtools. Thanks
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-2 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="chat-message">
-                    <div className="flex items-end">
-                      <div className="order-2 mx-2 flex max-w-xs flex-col items-start space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg rounded-bl-none bg-gray-300 px-4 py-2 text-gray-600">
-                            Thanks for your message David. I thought Im alone
-                            with this issue. Please, ? the issue to support it
-                            :)
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-1 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="chat-message">
-                    <div className="flex items-end justify-end">
-                      <div className="order-1 mx-2 flex max-w-xs flex-col items-end space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg bg-blue-600 px-4 py-2 text-white ">
-                            Are you using sudo?
-                          </span>
-                        </div>
-                        <div>
-                          <span className="inline-block rounded-lg rounded-br-none bg-blue-600 px-4 py-2 text-white ">
-                            Run this command sudo chown -R `whoami` /Users/{"{"}
-                            {"{"}your_user_profile{"}"}
-                            {"}"}/.npm-global/ then install the package globally
-                            without using sudo
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-2 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="chat-message">
-                    <div className="flex items-end">
-                      <div className="order-2 mx-2 flex max-w-xs flex-col items-start space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg bg-gray-300 px-4 py-2 text-gray-600">
-                            It seems like you are from Mac OS world. There is no
-                            /Users/ folder on linux ?
-                          </span>
-                        </div>
-                        <div>
-                          <span className="inline-block rounded-lg rounded-bl-none bg-gray-300 px-4 py-2 text-gray-600">
-                            I have no issue with any other packages installed
-                            with root permission globally.
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-1 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="chat-message">
-                    <div className="flex items-end justify-end">
-                      <div className="order-1 mx-2 flex max-w-xs flex-col items-end space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg rounded-br-none bg-blue-600 px-4 py-2 text-white ">
-                            yes, I have a mac. I never had issues with root
-                            permission as well, but this helped me to solve the
-                            problem
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-2 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="chat-message">
-                    <div className="flex items-end">
-                      <div className="order-2 mx-2 flex max-w-xs flex-col items-start space-y-2 text-xs">
-                        <div>
-                          <span className="inline-block rounded-lg bg-gray-300 px-4 py-2 text-gray-600">
-                            I get the same error on Arch Linux (also with sudo)
-                          </span>
-                        </div>
-                        <div>
-                          <span className="inline-block rounded-lg bg-gray-300 px-4 py-2 text-gray-600">
-                            I also have this issue, Here is what I was doing
-                            until now: #1076
-                          </span>
-                        </div>
-                        <div>
-                          <span className="inline-block rounded-lg rounded-bl-none bg-gray-300 px-4 py-2 text-gray-600">
-                            even i am facing
-                          </span>
-                        </div>
-                      </div>
-                      <img
-                        src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=144&h=144"
-                        alt="My profile"
-                        className="order-1 h-6 w-6 rounded-full"
-                      />
-                    </div>
-                  </div> */}
                   <div className="chat-message">
                     <div className="flex items-end">
                       <div className="order-2 mx-2 flex max-w-xs flex-col items-start space-y-2 text-xs">
@@ -685,9 +505,16 @@ export async function getServerSideProps(context: any) {
       return [];
     });
 
+  let chat = await prisma.chat.findMany({
+    where: {
+      userId: session.user?.id,
+    },
+  });
+
   return {
     props: {
       pdf_files,
+      chat,
     },
   };
 }
